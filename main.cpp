b3f868eac4f75bcdfb7019d6fd79b77427d4abc1
adityasusawat <akablackdevils300@gmail.com>
2023-11-02 15:17:18 +0530

    Adding existing project files to git hub

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main(){
+    // Image
+
+    int image_width = 256;
+    int image_height = 256;
+
+    // Render
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    //P3 = colours are in ASCII, image_width columns and image_width rows, 255 is the max colour, then we define the RGB triplets in the for loop
+
+    for (int j = 0; j < image_height; ++j) {
+        for (int i = 0; i < image_width; ++i) {
+            auto r = double(i) / (image_width-1);
+            auto g = double(j) / (image_height-1);
+            auto b = 0;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+}
